39fa9e496ea3c6a1de4445a590572a867b9ee6f7
Chavva-HasyaReddy <153480170+Chavva-HasyaReddy@users.noreply.github.com>
2023-12-17 19:06:48 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+//Class Name and File Name may be same if the class is a public class
+//If the class is not a public class, then it's may have different names
+// During compilation, file name will be used as javac Test.java
+// For running class name will be used as java Hello
+
+class Hello
+{
+  public static void main(String args[])
+  {
+     System.out.print("Hello World");
+  }
+}
